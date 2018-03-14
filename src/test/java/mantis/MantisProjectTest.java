4b8f434808f8edc3473512ab382c0441f62ff9c2
Dmitry <dmitry.trok@gmail.com>
2018-03-14 15:00:05 +0300

    Add Create project test

---
+++ src/test/java/mantis/MantisProjectTest.java
@@ -0,0 +1,15 @@
+package mantis;
+
+import org.junit.Test;
+
+/**
+ * Created by dmitry on 14.03.2018.
+ */
+public class MantisProjectTest {
+
+    @Test
+    public void createProjectTest() {
+        System.out.println("Create project test!");
+    }
+
+}
